e666871a2564f4269de9bd7eb2ffbd6056591c28
sujikim <sujikim@student.42seoul.kr>
2023-03-16 21:01:24 +0900

    [FIX] eval repository 한꺼번에 DB 저장하도록 변경

--- src/eval/eval.repository.ts
+++ src/eval/eval.repository.ts
@@ -1,15 +1,14 @@
-import { Repository } from "typeorm";
-import { EvalEntity } from "./eval.entity";
+import { Repository } from 'typeorm';
+import { EvalEntity } from './eval.entity';
 import { CustomRepository } from 'src/typeorm-ex/typeorm-ex.decorator';
-import { EvalDtoType } from "./eval.dto";
+import { EvalDtoType } from './eval.dto';
 
 @CustomRepository(EvalEntity)
 export class EvalRepository extends Repository<EvalEntity> {
+  async createEvalDatas(evalData: EvalEntity[]): Promise<EvalEntity[]> {
+    const data = this.create(evalData);
 
-	async createEvalData(evalData: EvalEntity): Promise<EvalEntity> {
-		const data = this.create(evalData);
-
-		await this.save(data);
-		return (data);
-	}
+    await this.save(data);
+    return data;
+  }
 }
--- src/eval/eval.service.ts
+++ src/eval/eval.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from "typeorm";
+import { Repository } from 'typeorm';
 import { EvalRepository } from './eval.repository';
 import { EvalEntity } from './eval.entity';
 import { ApiService } from 'src/api/api.service';
@@ -8,27 +8,20 @@ import { EvalDtoType } from './eval.dto';
 
 @Injectable()
 export class EvalService {
+  constructor(
+    @InjectRepository(EvalRepository)
+    private evalRepository: EvalRepository,
+    private apiService: ApiService,
+  ) {}
 
-	constructor (
-		@InjectRepository(EvalRepository)
-		private evalRepository: EvalRepository,
-		private apiService:ApiService,
-	) {}
-
-	async createEvalData(evalDataDtos: EvalDtoType[]): Promise <void> {
-		console.log(evalDataDtos.flag)
-		// const evalEntitys = evalDataDtos.map(({id, comment, begin_at, filled_at,project_id, corrector, duration,flags }) => {
-
-			// const isOut = flags.filter(x=>x.id===9)[0].positive === true ? true: false || false
-			// return {id, comment, beginAt:begin_at, filledAt:filled_at, projectId:project_id,from:corrector.id, duration, isOutStanding:isOut};
-		// })
-		// console.log(evalEntitys);
-		// console.dir(evalDataDtos.map(i => i.flags.filter(i => i.id === 9)));
-
-			// await this.evalRepository.createEvalData(evalData);
-	}
+  async createEvalData(evalData: EvalEntity[]): Promise<void> {
+    try {
+      await this.evalRepository.createEvalDatas(evalData);
+    } catch (e) {
+      console.log(e.message);
+    }
+  }
 }
-
 
 /*
 
