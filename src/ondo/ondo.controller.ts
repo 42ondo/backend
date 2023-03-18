76093d689b77c939e517f693a3719b03c80acac1
sujikim <sujikim@student.42seoul.kr>
2023-03-19 08:44:41 +0900

    [FIX]] api 명세와 다른 요청 및 응답값 수정

--- src/ondo/ondo.controller.ts
+++ src/ondo/ondo.controller.ts
@@ -1,19 +1,18 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { OndoService } from './ondo.service';
 
 @Controller('ondo')
 export class OndoController {
+  constructor(private ondoService: OndoService) {}
 
-	constructor (private ondoService: OndoService) {}
-
-
-	@Get('/average')
-	async handler2 ():Promise<any> { // renaming
-		console.log("test");
-		return (await this.ondoService.getOndoAverage());
-	}
-	@Get('/rank/:count')
-	async handler (@Param('count') count:number) {
-		return await this.ondoService.getOndoRank(count);
-	}
+  @Get('/average')
+  async handler2(): Promise<any> {
+    // renaming
+    return await this.ondoService.getOndoAverage();
+  }
+  @Get('/rank')
+  async handler(@Query('count') count: number) {
+    const users = await this.ondoService.getOndoRank(count);
+    return { users };
+  }
 }
