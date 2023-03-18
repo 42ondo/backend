7e4ce95f8f4dd4bf715fc19b9055fefc2ff91c09
dongyshi <sdyeob1002@gmail.com>
2023-03-19 03:25:56 +0900

    [FEAT] API 명세 1번 추가

--- src/ondo/ondo.controller.ts
+++ src/ondo/ondo.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { OndoService } from './ondo.service';
 
 @Controller('ondo')
@@ -12,9 +12,8 @@ export class OndoController {
 		console.log("test");
 		return (await this.ondoService.getOndoAverage());
 	}
-	@Get('/ondo/rank')
-	handler () { // renaming
-		// return this.ondoService.getOndoRank();
+	@Get('/rank/:count')
+	async handler (@Param('count') count:number) {
+		return await this.ondoService.getOndoRank(count);
 	}
-
 }
--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -9,4 +9,23 @@ export class UserRepository extends Repository<UserEntity> {
 
     await this.save(data);
   }
+
+	async getOndoRank(count: number): Promise<{id: number, name: string, imgUrl: string, ondo: number}[]> {
+		const topWords = await this
+			.createQueryBuilder('user_entity')
+			.select('user_entity.ondo', 'ondo')
+			.addSelect('user_entity.login', 'login')
+			.addSelect('user_entity.imgUrl', 'imgUrl')
+			.addSelect('user_entity.id', 'id')
+			.orderBy('user_entity.ondo', 'DESC')
+			.limit(count)
+			.getRawMany();
+		console.log(topWords);
+		return topWords.map((result) => ({ 
+			id: result.id, 
+			name: result.login, 
+			imgUrl: result.imgUrl, 
+			ondo: result.ondo 
+		}));
+	}
 }
