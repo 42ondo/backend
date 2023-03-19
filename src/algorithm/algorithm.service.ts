77663a2f613d79bf7768f9fd374449f5e3a0d32a
Gori <wnsrjf0922@gmail.com>
2023-03-19 12:17:17 +0900

    [FEAT] 온도 알고리즘 구현

--- src/algorithm/algorithm.service.ts
+++ src/algorithm/algorithm.service.ts
@@ -5,9 +5,9 @@ import { EvalRepository } from "src/eval/eval.repository";
 import { UserEntity } from "src/user/user.entity";
 import { UserRepository } from "src/user/user.repository";
 import { WordRepository } from "src/word/word.repository";
-import { WordList } from "src/algorithm/algorithm.wordlist"
+import { WordList as wordList } from "src/algorithm/algorithm.wordlist"
 
-const Ratio = 0.3;
+const Ratio = 0.4;
 
 type EvalList = {
     commentLength: number,
@@ -24,7 +24,7 @@ export class AlgorithmService {
         private userRepository: UserRepository,
         @InjectRepository(WordRepository)
         private wordRepository: WordRepository,
-    ) {}
+    ) {}	
 
 	public async function(datas: EvalEntity[]): Promise<void> {
 		for (const data of datas) {
@@ -33,9 +33,15 @@ export class AlgorithmService {
 	}
 
 	async function2 (data: EvalEntity): Promise<void> {
-		let weightToOndo: number;
+		let weightToOndo: number = 0;
 		let begin = new Date(data.beginAt);
 		let filled = new Date(data.filledAt);
+
+		const userInf = await this.userRepository.findOne({
+			where: {
+				id: data.from,
+			}
+		})
 
 		const evalList: EvalList = {
 			commentLength: data.comment.length,
@@ -43,22 +49,19 @@ export class AlgorithmService {
 			timeSpent: filled.getTime() - begin.getTime()
 		}
 
-		//1. wordRepo에서 해당 data에 관련된(user_id를 이용해서) word정보 추출.: []
 		const usedWordList = await this.wordRepository.find({
 			where: {
 				user_id: data.from,
 			}
 		})
 		
-		//2. 추출된 단어정보를 가지고 가중치 계산 (whitelist blacklist 들어갔다 나오면서)
-		const wordList = new WordList();
 		const globalW = wordList.globalWhite.element;
 		const globalB = wordList.globalBlack.element;
 		let cnt = 0;
-		for (const [key, value] of Object(wordList).entries() ) {
+		for (const [key, value] of Object.entries(wordList) ) {
 			if (value.project_id === data.projectId) {
 				usedWordList.forEach(x=>{
-					if (value.project_id.element.includes(x.word))
+					if (value.element.includes(x.word))
 						cnt++;
 					if (globalW.includes(x.word))
 						cnt++;
@@ -68,26 +71,24 @@ export class AlgorithmService {
 			}
 			break ;
 		}
+
 		weightToOndo += cnt * Ratio 
-		//4. timeSpent변수를 가지고, 가중치 계산
-		//5. TimeScore 변수에 가중치 할당
 		if (evalList.timeSpent / data.duration < 3) {
 			const n = evalList.timeSpent / data.duration - 1
 			weightToOndo += n * Ratio;
 		}
-
-		// commnetLength값으로 가중치
-		if (evalList.commentLength > 60) {
-			weightToOndo += (evalList.commentLength / 60) * Ratio
-		} else {
-			weightToOndo -= (evalList.commentLength / 60) * Ratio
-		}
-
-		//7. IsOutStanding값을 가지고 가중치 계산 및 할당.
+		weightToOndo += ((evalList.commentLength / 200) - 2) * Ratio
 		if (evalList.isOutStanding === true) {
 			weightToOndo += 1 * Ratio
 		}
-
-		 this.userRepository.update({id: data.id}, {ondo: + weightToOndo})
+		let newOndo = userInf.ondo + weightToOndo;
+		
+		if (newOndo >= 99) {
+			newOndo = 99;
+		} else if (newOndo <= 0) {
+			newOndo = 0;
+		}
+		console.log(userInf.ondo, weightToOndo,newOndo);
+		this.userRepository.update({id: data.from}, {ondo: newOndo})
 	}
 }
